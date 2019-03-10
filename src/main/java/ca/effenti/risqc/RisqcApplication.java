237a1ff28588e59220400958a05f04a7ed43969e
roland <you@example.com>
2019-03-10 00:17:21 -0500

    initial commit

---
+++ src/main/java/ca/effenti/risqc/RisqcApplication.java
@@ -0,0 +1,13 @@
+package ca.effenti.risqc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RisqcApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(RisqcApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ca/effenti/risqc/controller/RisksController.java
@@ -0,0 +1,22 @@
+package ca.effenti.risqc.controller;
+
+import ca.effenti.risqc.model.Message;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class RisksController {
+
+    private static final String MESSAGE = "Hello, World!";
+
+    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE})
+    public Message hello() {
+        Message message = new Message();
+        message.setMessage(MESSAGE);
+        return message;
+    }
+
+}
